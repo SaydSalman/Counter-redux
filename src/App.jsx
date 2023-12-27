b0e37fcaf1af08e7840f58dcdd5db949ee7a1d7b
Sayyucha <119330669+Sayyucha@users.noreply.github.com>
2023-12-27 12:11:54 +0530

    cnt

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import Counter from './Components/Counter'
+
+function App() {
+  
+ 
+
+  return (
+    <div style={{height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center'>
+     <Counter/>
+    </div>
+  )
+}
+
+export default App
